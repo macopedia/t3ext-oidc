fa042fc223ba6e01e5128255434dc9d1d2394be1
Andrzej Kołbuc <a.kolbuc@macopedia.com>
2020-10-26 11:03:14 +0100

    Update php and typo3 requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,8 +21,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.5.0',
     'constraints' => [
         'depends' => [
-            'php' => '7.0.0-7.3.99',
-            'typo3' => '8.7.0-9.5.99',
+            'php' => '7.0.0-7.4.99',
+            'typo3' => '9.5.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
